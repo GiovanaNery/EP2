fc0157935a8a5216f83723dc88181b508cea02d7
GiovanaNery <130416807+GiovanaNery@users.noreply.github.com>
2023-04-25 19:27:31 -0300

    preenche frota

---
+++ 2_preenche_frota.py
@@ -0,0 +1,27 @@
+def define_posicoes(linha, coluna, orientacao, tamanho): 
+    posicoes = []
+    if orientacao == 'vertical': 
+        for i in range(tamanho): 
+            posicoes.append([linha+i, coluna])
+    elif orientacao == 'horizontal': 
+        for i in range(tamanho): 
+            posicoes.append([linha, coluna+i])
+    return posicoes
+
+
+
+def preenche_frota(frota, nome_navio, linha, coluna, orientacao, tamanho):
+    if nome_navio in frota: 
+        frota[nome_navio].append(define_posicoes(linha, coluna, orientacao, tamanho))
+    else: #se o nome do navio nao esta no dicio
+        frota[nome_navio] = [define_posicoes(linha, coluna, orientacao, tamanho)]
+    return frota 
+    
+    
+    
+        
+     
+
+
+    
+    
