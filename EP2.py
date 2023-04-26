12137770e1e3729e349f12fa076f1b3d28ec13f9
isadoramilanez <130523144+isadoramilanez@users.noreply.github.com>
2023-04-26 09:06:01 -0300

    ex 6 (posição valida) no EP2

--- EP2.py
+++ EP2.py
@@ -68,6 +68,21 @@ def afundados(frota, tabuleiro):
 
 
 
+def posicao_valida(frota, linha, coluna, orientacao, tamanho):
+    posicoes = define_posicoes(linha, coluna, orientacao, tamanho)
+    for posicao in posicoes:
+        if posicao[0]<0 or posicao[1]<0 or posicao[0]>=10 or posicao[1]>=10:
+            return False
+        for coordenadas in frota.values():
+            for j in range (len(coordenadas)):
+                if posicao in coordenadas[j]:
+                    return False
+    if frota == {}:
+        return True 
+    return True   
+
+
+
           
 
     
